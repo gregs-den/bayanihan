4f96b4f332da53cec65a0972dba9961025e5fa05
greg <haroldsalvilla@gmail.com>
2026-08-19 16:35:53 +0800

    Fix trailing slash in CORS origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'https://bayanihan-indol.vercel.app/',
+    origin: [
+      'https://bayanihan-indol.vercel.app',
+      'http://localhost:3001',
+    ]
   });
   await app.listen(process.env.PORT ?? 3000);
 }
